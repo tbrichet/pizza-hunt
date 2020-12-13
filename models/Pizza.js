9c3e1aecde62e1b0f1e7168e81ce8957f5f3fcbb
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-12-13 07:35:31 -0700

    final

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,12 +3,16 @@ const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema(
     {
-      pizzaName: {
-        type: String
-      },
-      createdBy: {
-        type: String
-      },
+        pizzaName: {
+            type: String,
+            required: 'Please provide a pizza name!',
+            trim: true
+          },
+          createdBy: {
+            type: String,
+            required: 'Please provide your name!',
+            trim: true
+          },
       createdAt: {
         type: Date,
         default: Date.now,
@@ -17,6 +21,8 @@ const PizzaSchema = new Schema(
       },
       size: {
         type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
         default: 'Large'
       },
       toppings: [],
